c45d6b713db8c688eb53154ed2289e4f8fb32805
Amir Faisal <amirfaisallinkon2@gmail.com>
2025-12-19 02:13:28 +0600

    fix init_db order final

--- app.py
+++ app.py
@@ -345,7 +345,7 @@ def check_license():
         'amount': license['amount']
     })
 
-if __name__ == '__main__':
-    init_db()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+init_db()
 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
